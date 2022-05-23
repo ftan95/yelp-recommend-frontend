9d6475907bb052d976ded5ace8ee5aa0cc6c8e5c
Frederick Tan <ftan2@scu.edu>
2022-05-23 16:22:46 -0700

    pushing frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import placeholder from './placeholder.png';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const userId = [
+    'yhByTmJlswtYvSq9qKrskQ',
+    'gBTEAlR4UdxiVOF_3oui0A',
+    'Pp-T524lINokAcXw3qODKw',
+    'tdNV2Wb0LrFpm1Yfov_Klw',
+    'i-dWog1af9Q6WBm68By9Vg',
+    'R9FyRhL305takxCWbzstvg',
+    'z92s6ue5ql2BFe02W8Q7-A',
+    'vlSA47FlfINOpooclRc7VQ',
+    'HCFkEJRE2PJkkrIvZDrIOg',
+    '-Y0JQ2lorYCZVNkFJFAZAA'
+  ];
+
+  // const [busId, setBusId] = useState([]);
+  var randomId = userId[Math.floor(Math.random()*userId.length)];;
+  const [busInfoList, setBusInfoList] = useState([]);
+
+  useEffect(() => {
+    fetchData(randomId);
+    // console.log('i fire once');
+  }, []);
+
+  function fetchData(randomId) {
+    fetch(`http://ec2-52-53-159-85.us-west-1.compute.amazonaws.com:8000/recommend?` + new URLSearchParams({user: randomId}).toString(), {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      const transformedId = data[0].map((business, index) => {
+        return {
+          id: index,
+          busId: business
+        }
+      });
+      transformedId.forEach((business) => {
+        fetch(`http://52.53.159.85:8000/searchbusiness?` + new URLSearchParams({business: business.busId}).toString(), {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then((data) => {
+          // console.log(data);
+          const transformedInfo = ({
+            id: data[0],
+            name: data[5],
+            location: data[2] + ", " + data[3],
+            reviews: data[4],
+            rating: data[6]
+          });
+          setBusInfoList(prev => [...prev, transformedInfo]);
+        })
+      })
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Container>
+          {busInfoList.map(bus => 
+            <Row className='recommend' key={bus.id}>
+              <Col>
+                <img src={placeholder} className="App-logo" alt="logo" />
+              </Col>
+              <Col>
+                  <h1>{bus.name}</h1>
+                  <p>Rating: {bus.rating}</p>
+                  <p>Location: {bus.location}</p>
+                  <p>Reviews: {bus.reviews}</p>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      </div>
     </div>
   );
 }
